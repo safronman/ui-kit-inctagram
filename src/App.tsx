d51f5d06f6b9e49176c99318b408b0bb23cc2063
safronman <safronmanbox@gmail.com>
2026-02-24 11:06:12 +0300

    refactor: use typography heading in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button'
+import { H1 } from '@/components/ui/typography.tsx'
+import { Button } from '@/components/ui/button.tsx'
 
 function App() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-background p-6">
       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-2xl font-semibold text-foreground">Shadcn UI setup</h1>
+        <H1>Shadcn UI setup</H1>
         <Button variant={'default'}>Shadcn Button</Button>
       </div>
     </main>
