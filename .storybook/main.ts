a1c492a4c8bec0a908f0906b798e274eaeac52b5
safronman <safronmanbox@gmail.com>
2026-02-24 16:45:46 +0300

    chore: add storybook mcp addon and fix ts config resolution

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,25 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@chromatic-com/storybook', '@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-onboarding'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    {
+      name: '@storybook/addon-mcp',
+      options: {
+        toolsets: {
+          dev: true,
+          docs: true,
+        },
+        experimentalFormat: 'markdown',
+      },
+    },
+  ],
+  features: {
+    experimentalComponentsManifest: true,
+  },
   framework: '@storybook/react-vite',
 }
 export default config
